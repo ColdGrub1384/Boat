b48a40834f1375e174ffc28ce45998b8a2b9cc4a
Adrian Labbe <adrilabbelol@gmail.com>
2018-05-21 19:31:27 -0400

    Initial Commit

---
+++ Boat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boat
+//
+//  Created by Adrian on 21.05.18.
+//  Copyright © 2018 Adrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

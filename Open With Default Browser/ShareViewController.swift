50837145c679906ee5899a31933d9b7ff211473a
Adrian Labbe <adrilabbelol@gmail.com>
2018-06-02 13:03:53 -0400

    Show error in Share extension if notifications are disabled

--- Open With Default Browser/ShareViewController.swift
+++ Open With Default Browser/ShareViewController.swift
@@ -73,6 +73,52 @@ class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        func setup() {
+            func complete() {
+                extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+            }
+            
+            guard let items = extensionContext?.inputItems as? [NSExtensionItem] else {
+                complete()
+                return
+            }
+            
+            if items.count == 0 {
+                complete()
+                return
+            }
+            
+            for item in items {
+                if let itemProviders = item.attachments as? [NSItemProvider] {
+                    for itemProvider in itemProviders {
+                        if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeText as String) {
+                            itemProvider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil) { (data, _) in
+                                if let string = data as? String, let url = URL(string: string), url.scheme == "http" || url.scheme == "https" {
+                                    self.sendNotification(withURL: url)
+                                } else if let string = data as? String, let encoded = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: "https://www.google.com/search?q="+encoded) {
+                                    self.sendNotification(withURL: url)
+                                } else {
+                                    complete()
+                                }
+                            }
+                        } else if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+                            itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) { (data, _) in
+                                if let url = data as? URL, url.scheme == "http" || url.scheme == "https" {
+                                    self.sendNotification(withURL: url)
+                                } else {
+                                    complete()
+                                }
+                            }
+                        } else {
+                            complete()
+                        }
+                    }
+                } else {
+                    complete()
+                }
+            }
+        }
+        
         UNUserNotificationCenter.current().getNotificationSettings { (settings) in
             if settings.authorizationStatus != .authorized || settings.alertSetting == .disabled {
                 self.doneButton.isHidden = false
@@ -80,50 +126,8 @@ class ShareViewController: UIViewController {
                 self.messageLabel.isHidden = false
                 self.messageLabel.text = Localizable.enableNotificationsAlert
                 return
-            }
-        }
-        
-        func complete() {
-            extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
-        }
-        
-        guard let items = extensionContext?.inputItems as? [NSExtensionItem] else {
-            complete()
-            return
-        }
-        
-        if items.count == 0 {
-            complete()
-            return
-        }
-        
-        for item in items {
-            if let itemProviders = item.attachments as? [NSItemProvider] {
-                for itemProvider in itemProviders {
-                    if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeText as String) {
-                        itemProvider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil) { (data, _) in
-                            if let string = data as? String, let url = URL(string: string), url.scheme == "http" || url.scheme == "https" {
-                                self.sendNotification(withURL: url)
-                            } else if let string = data as? String, let encoded = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: "https://www.google.com/search?q="+encoded) {
-                                self.sendNotification(withURL: url)
-                            } else {
-                                complete()
-                            }
-                        }
-                    } else if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                        itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) { (data, _) in
-                            if let url = data as? URL, url.scheme == "http" || url.scheme == "https" {
-                                self.sendNotification(withURL: url)
-                            } else {
-                                complete()
-                            }
-                        }
-                    } else {
-                        complete()
-                    }
-                }
             } else {
-                complete()
+                setup()
             }
         }
     }
